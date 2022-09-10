b4fef628dab893b584aed2613f82cb35adfd2188
YapingXin <yaping.xin@foxmail.com>
2022-09-10 09:52:19 +0800

    Additional Include Directories: [1] ../../../../../Code [2] ../../../../../Code/Utilities/Tools/TelemSIM

--- Code/Utilities/Tools/TelemSIM/TelemSIM.c
+++ Code/Utilities/Tools/TelemSIM/TelemSIM.c
@@ -4,8 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "Common/datatypes.h"
+#include "Common/PosixInterface.h"
+
+
 int main(int argc, char* argv[])
 {
+    HResult retcode = HResult_OK;
+
+    if (retcode != HResult_OK)
+    {
+        printf("[ERROR] service_stop() failed.\n");
+        goto EXIT;
+    }
+
+EXIT:
     return EXIT_SUCCESS;
 }
 
