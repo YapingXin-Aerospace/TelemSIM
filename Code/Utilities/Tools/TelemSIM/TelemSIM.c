60d038125e5d56086cc40b86c3b7535e6d5ecd46
YapingXin <yaping.xin@foxmail.com>
2022-09-10 09:03:46 +0800

    Project Win32/Tools/TelemSIM/TelemSIM.vcxproj initial upload.

---
+++ Code/Utilities/Tools/TelemSIM/TelemSIM.c
@@ -0,0 +1,11 @@
+// TelemSIM.c : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    return EXIT_SUCCESS;
+}
+
