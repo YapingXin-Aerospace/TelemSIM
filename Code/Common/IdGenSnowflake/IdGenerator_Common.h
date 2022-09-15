0ba7c2ffec3f0bc60693a303b55af9030d04d403
Yaping Xin (Aerospace) <yaping.xin@foxmail.com>
2022-09-15 15:05:32 +0800

    Remove IdGenSnowflake/

--- Code/Common/IdGenSnowflake/IdGenerator_Common.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INC_COMMON_IDGENSNOWFLAKE_IdGenerator_Common_H
-#define _INC_COMMON_IDGENSNOWFLAKE_IdGenerator_Common_H
-
-#include "Common/datatypes.h"
-
-int64_t GetCurrentTime();
-
-#endif // !_INC_COMMON_IDGENSNOWFLAKE_IdGenerator_Common_H
