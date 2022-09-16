d182f9322d10bd9a9c5ae5db59423b018ff4f67c
Yaping Xin (Aerospace) <yaping.xin@foxmail.com>
2022-09-16 17:45:14 +0800

    TM_PreLoad.h | StepLogSet* get_global_StepLogSet()

--- Code/Common/TMLib/TM_PreLoad.c
+++ Code/Common/TMLib/TM_PreLoad.c
@@ -1,1 +1,9 @@
 #include "TM_PreLoad.h"
+
+static StepLogSet G_StepLogSet = { 0 };
+
+
+StepLogSet* get_global_StepLogSet()
+{
+    return &G_StepLogSet;
+}
--- Code/Common/TMLib/TM_PreLoad.h
+++ Code/Common/TMLib/TM_PreLoad.h
@@ -1,6 +1,9 @@
 #ifndef _INC_COMMON_TMLIB_TM_PreLoad_H
 #define _INC_COMMON_TMLIB_TM_PreLoad_H
 
+#include "TM_Common.h"
+
+StepLogSet* get_global_StepLogSet();
 
 
 #endif // !_INC_COMMON_TMLIB_TM_PreLoad_H
